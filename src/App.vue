a8d3dac2a9dac00cc66427cb56b97d956f6e3ac4
DESKTOP-QPBSKD8\Playdata <ahning1203@gmail.com>
2024-09-27 17:53:52 +0900

    feat:main page

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,9 @@ Coded by www.creative-tim.com
 import { computed } from "vue";
 import { useStore } from "vuex";
 import Sidenav from "./examples/Sidenav";
-// import Configurator from "@/examples/Configurator.vue";
+import Configurator from "@/examples/Configurator.vue";
 import Navbar from "@/examples/Navbars/Navbar.vue";
-// import AppFooter from "@/examples/Footer.vue";
+import AppFooter from "@/examples/Footer.vue";
 
 const store = useStore();
 const isNavFixed = computed(() => store.state.isNavFixed);
@@ -27,10 +27,10 @@ const isAbsolute = computed(() => store.state.isAbsolute);
 const showSidenav = computed(() => store.state.showSidenav);
 const layout = computed(() => store.state.layout);
 const showNavbar = computed(() => store.state.showNavbar);
-// const showFooter = computed(() => store.state.showFooter);
-// const showConfig = computed(() => store.state.showConfig);
-// const hideConfigButton = computed(() => store.state.hideConfigButton);
-// const toggleConfigurator = () => store.commit("toggleConfigurator");
+const showFooter = computed(() => store.state.showFooter);
+const showConfig = computed(() => store.state.showConfig);
+const hideConfigButton = computed(() => store.state.hideConfigButton);
+const toggleConfigurator = () => store.commit("toggleConfigurator");
 
 const navClasses = computed(() => {
   return {
@@ -57,13 +57,13 @@ const navClasses = computed(() => {
 
     <navbar :class="[navClasses]" v-if="showNavbar" />
 
-<!--    <router-view />-->
+    <router-view />
 
 <!--    <app-footer v-show="showFooter" />-->
 
-<!--    <configurator-->
-<!--      :toggle="toggleConfigurator"-->
-<!--      :class="[showConfig ? 'show' : '', hideConfigButton ? 'd-none' : '']"-->
-<!--    />-->
+    <configurator
+      :toggle="toggleConfigurator"
+      :class="[showConfig ? 'show' : '', hideConfigButton ? 'd-none' : '']"
+    />
   </main>
 </template>
--- src/views/components/ContentsDropdown.vue
+++ src/views/components/ContentsDropdown.vue
@@ -1,11 +1,40 @@
-<script setup>
-
-</script>
-
 <template>
-  $END$
+  <div class="dropdown">
+    <button @click="toggleDropdown" class="btn btn-sm dropdown-toggle mb-0 shadow-blur" type="button">
+      {{ selected || 'Select' }}
+    </button>
+    <div v-if="isOpen" class="dropdown-menu show">
+      <div
+          class="dropdown-item"
+          v-for="option in options"
+          :key="option"
+          @click="selectColumn(option)"
+      >
+        {{ option }}
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<script>
+export default {
+  props: ['options'],
+  data() {
+    return {
+      isOpen: false,
+      selected: ''
+    };
+  },
+  methods: {
+    toggleDropdown() {
+      this.isOpen = !this.isOpen;
+    },
+    selectColumn(option) {
+      this.selected = option;
+      this.$emit('columnSelected', option);
+      this.isOpen = false; // 드롭다운 닫기
+    }
+  }
+};
+</script>
 
-</style>
