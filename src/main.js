f3d59463e4d08002df5c5a6d2cfecced638b5ff3
박성준 <macleod.park@gmail.com>
2024-10-11 11:40:00 +0900

    feat: axios interceptor 추가

--- src/main.js
+++ src/main.js
@@ -10,6 +10,35 @@ import ArgonDashboard from "./argon-dashboard";
 
 import axios from 'axios';
 axios.defaults.baseURL = 'http://localhost:8080/v1/api';
+axios.interceptors.request.use((config) => {
+    if(localStorage.getItem('user')){
+        const token = JSON.parse(localStorage.getItem('user'))['state']['accessToken'];
+        if(token){
+            config.headers.authorization = token;
+            config.withCredentials = true;
+    }
+    }
+    return config;
+});
+
+axios.interceptors.response.use(
+    (response) => {
+        const token = response.headers.authorization;
+        if(token){
+            const user = JSON.parse(localStorage.getItem('user'));
+            if(user){
+                user['state']['accessToken'] = token;
+                localStorage.setItem('user', JSON.stringify(user));
+            }
+        }
+        return response;
+    },
+    (error) => {
+        if(error.response.status == 401){
+            router.push('/');
+        }
+    }
+);
 
 const appInstance = createApp(App);
 const pinia = createPinia();
