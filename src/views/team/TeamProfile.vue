7b7984536540fed1f6b27a3178633ff169705e87
byHyen <chi0666@naver.com>
2024-11-04 17:05:13 +0900

    refactor: 팀원 삭제 모달창 삭제 및 권한 추가

--- src/views/team/TeamProfile.vue
+++ src/views/team/TeamProfile.vue
@@ -2,88 +2,68 @@
   <main>
     <div class="container-fluid profile-overview">
       <div class="card-list">
-        <!-- <div class="row justify-content-start"> -->
-          <!-- <div class="col-md-10"> -->
-            <!-- 팀 프로필 카드 -->
-            <div class="card" style="min-height: 150px;">
-              <div class="card-body p-3">
-                <!-- <div class="row gx-4"> -->
-                  <div class="col-auto">
-                    <div class="avatar avatar-xl position-relative mt-2">
-                      <img
-                        :src="selectedTeam.imageUrl ? selectedTeam.imageUrl : require('@/assets/img/default/profile.png')"
-                        class="w-100 border-radius-lg"
-                      />
-                    </div>
-                  </div>
-                  <div class="col-auto my-auto">
-                    <div class="h-100">
-                      <h5 class="mb-1">{{ selectedTeam.name }}</h5>
-                      <!-- <p class="mb-0 font-weight-bold text-sm">{{ selectedTeam.role }}</p> -->
-                    </div>
-                  </div>
-                <!-- </div> -->
+        <div class="card" style="min-height: 150px;">
+          <div class="card-body p-3">
+            <div class="col-auto">
+              <div class="avatar avatar-xl position-relative mt-2">
+                <img
+                  :src="selectedTeam.imageUrl ? selectedTeam.imageUrl : require('@/assets/img/default/profile.png')"
+                  class="w-100 border-radius-lg"
+                />
               </div>
             </div>
-
-            <!-- 팀원 리스트 -->
-            <div class="card mt-4" style="min-height: 425px;">
-              <div class="card-header pb-0 mt-2 d-flex justify-content-between align-items-center" style="margin: 0 3rem;">
-                <h5 class="mb-0">Member</h5>
-                <button class="btn btn-custom btn-sm" @click="showInviteModal">
-                  팀원 추가
-                </button>
-              </div>
-              <div class="card-body" style="margin: 0 3rem;">
-                <!-- 그룹장 섹션 -->
-                <div v-if="leader" class="group-leader">
-                  <h6 class="text-uppercase">그룹장</h6>
-                  <div class="profile-box position-relative">
-                    <img :src="leader.profileImage ? leader.profileImage : require('@/assets/img/default/profile.png')" class="member-icon" />
-                    <span class="member-name">{{ leader.nickname }}</span>
-                  </div>
-                  <hr class="horizontal dark" />
-                </div>
-
-                <!-- 그룹원 섹션 -->
-                <div class="group-members mt-4">
-                  <h6 class="text-uppercase">그룹원</h6>
-                  <div v-for="member in members" :key="member.id" class="position-relative member-item">
-                    <div class="member-box">
-                      <img :src="member.profileImage ? member.profileImage : require('@/assets/img/default/profile.png')" class="member-icon" />
-                      <span class="member-name">{{ member.nickname }}</span>
-                      <span class="delete-icon"  @click="confirmDelete(member)">X</span>
-                    </div>
-                  </div>
-                  <hr class="horizontal dark" />
-                </div>
-              </div>
-            </div>
-
-            <!-- 팀원 추가 모달 -->
-            <div v-if="isModalVisible" class="modal">
-              <div class="modal-content">
-                <span class="close" @click="closeInviteModal">&times;</span>
-                <h6>팀원 초대하기</h6>
-                <input type="email" v-model="inviteEmail" placeholder="이메일 입력" class="form-control" />
-                <button class="btn btn-primary mt-3" @click="sendInvite">전송</button>
-              </div>
-            </div>
-
-            <!-- 팀원 삭제 확인 모달 -->
-            <div v-if="isDeleteModalVisible" class="modal">
-              <div class="modal-content">
-                <span class="close" @click="closeDeleteModal">&times;</span>
-                <h6>팀원을 삭제하시겠습니까?</h6>
-                <div class="d-flex justify-content-end mt-4">
-                  <button class="btn btn-danger me-3" @click="deleteMember">삭제</button>
-                  <button class="btn btn-secondary" @click="closeDeleteModal">취소</button>
-                </div>
+            <div class="col-auto my-auto">
+              <div class="h-100">
+                <h5 class="mb-1">{{ selectedTeam.name }}</h5>
               </div>
             </div>
           </div>
-        <!-- </div> -->
-      <!-- </div> -->
+        </div>
+
+        <!-- 팀원 리스트 -->
+        <div class="card mt-4" style="min-height: 425px;">
+          <div class="card-header pb-0 mt-2 d-flex justify-content-between align-items-center" style="margin: 0 3rem;">
+            <h5 class="mb-0">Member</h5>
+            <button class="btn btn-custom btn-sm" @click="showInviteModal">
+              팀원 추가
+            </button>
+          </div>
+          <div class="card-body" style="margin: 0 3rem;">
+            <!-- 그룹장 섹션 -->
+            <div v-if="leader" class="group-leader">
+              <h6 class="text-uppercase">그룹장</h6>
+              <div class="profile-box position-relative">
+                <img :src="leader.profileImage ? leader.profileImage : require('@/assets/img/default/profile.png')" class="member-icon" />
+                <span class="member-name">{{ leader.nickname }}</span>
+              </div>
+              <hr class="horizontal dark" />
+            </div>
+
+            <!-- 그룹원 섹션 -->
+            <div class="group-members mt-4">
+              <h6 class="text-uppercase">그룹원</h6>
+              <div v-for="member in members" :key="member.id" class="position-relative member-item">
+                <div class="member-box">
+                  <img :src="member.profileImage ? member.profileImage : require('@/assets/img/default/profile.png')" class="member-icon" />
+                  <span class="member-name">{{ member.nickname }}</span>
+                  <span class="delete-icon" @click="confirmDelete(member)">X</span>
+                </div>
+              </div>
+              <hr class="horizontal dark" />
+            </div>
+          </div>
+        </div>
+
+        <!-- 팀원 추가 모달 -->
+        <div v-if="isModalVisible" class="modal">
+          <div class="modal-content">
+            <span class="close" @click="closeInviteModal">&times;</span>
+            <h6>팀원 초대하기</h6>
+            <input type="email" v-model="inviteEmail" placeholder="이메일 입력" class="form-control" />
+            <button class="btn btn-primary mt-3" @click="sendInvite">전송</button>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -92,7 +72,7 @@
 import { ref, watch, onMounted } from 'vue';
 import axios from 'axios';
 import { useUserStore } from '@/store/user';
-import { useRoute, useRouter } from 'vue-router';
+import { useRoute } from 'vue-router';
 
 export default {
   props: {
@@ -105,10 +85,9 @@ export default {
     const members = ref([]);
     const inviteEmail = ref('');
     const isModalVisible = ref(false);
-    const isDeleteModalVisible = ref(false);
-    const isEditModalVisible = ref(false);
     const memberToDelete = ref(null);
     const route = useRoute();
+    const userStore = useUserStore();
 
     const separateMembers = async (teamMembers) => {
       if (teamMembers.length > 0) {
@@ -122,17 +101,15 @@ export default {
         const response = await axios.get(`/team/list/member?teamId=${route.params.id}`);
         const teamMembers = response.data || [];
         await separateMembers(teamMembers);
-        // console.log("teamMembers: ", teamMembers);
-        console.log("members.value: ", members.value);
       } catch (error) {
         console.error('팀원 목록을 불러오는 중 오류 발생:', error);
       }
     };
+
     const fetchTeamData = async () => {
       try {
         const response = await axios.get(`/team/${route.params.id}`);
         selectedTeam.value = response.data;
-        console.log(selectedTeam.value);
       } catch (error) {
         console.error('팀 정보를 불러오는데 실패했습니다:', error);
       }
@@ -148,16 +125,10 @@ export default {
       }
     );
 
-    onMounted(async() => (fetchTeamMembers()));
-    fetchTeamData();
-
-    const openEditModal = () => {
-      isEditModalVisible.value = true;
-    };
-
-    const closeEditModal = () => {
-      isEditModalVisible.value = false;
-    };
+    onMounted(async () => {
+      await fetchTeamMembers();
+      await fetchTeamData();
+    });
 
     const showInviteModal = () => {
       isModalVisible.value = true;
@@ -169,8 +140,6 @@ export default {
     };
 
     const sendInvite = async () => {
-      console.log("초대할 팀 ID:", selectedTeam.value.id);
-      console.log("초대할 이메일:", inviteEmail.value);
       try {
         await axios.post(`/team/${selectedTeam.value.id}/invite`, null, {
           params: { inviteeEmail: inviteEmail.value },
@@ -183,28 +152,27 @@ export default {
       } catch (error) {
         console.error('초대 이메일 전송 중 오류 발생:', error);
       }
-};
+    };
 
     const confirmDelete = (member) => {
-      memberToDelete.value = member;
-      isDeleteModalVisible.value = true;
+      if (userStore.userInfo.id !== leader.value.id) {
+        alert("팀원 삭제는 그룹장만 가능합니다.");
+        return;
+      }
+
+      if (confirm(`팀원 ${member.nickname}을(를) 삭제하시겠습니까?`)) {
+        deleteMember(member);
+      }
     };
 
-    const closeDeleteModal = () => {
-      isDeleteModalVisible.value = false;
-      memberToDelete.value = null;
-    };
-
-    const deleteMember = async () => {
+    const deleteMember = async (member) => {
       try {
-        await axios.delete(`/team/remove/${selectedTeam.value.id}/${memberToDelete.value.id}`);
-        members.value = members.value.filter((member) => member.id !== memberToDelete.value.id);
-        closeDeleteModal();
+        await axios.delete(`/team/remove/${selectedTeam.value.id}/${member.id}`);
+        members.value = members.value.filter((m) => m.id !== member.id);
       } catch (error) {
         console.error('팀원 삭제 중 오류 발생:', error);
       }
     };
-
 
     return {
       selectedTeam,
@@ -212,84 +180,70 @@ export default {
       leader,
       inviteEmail,
       isModalVisible,
-      isDeleteModalVisible,
-      isEditModalVisible,
-      openEditModal,
-      closeEditModal,
       showInviteModal,
       closeInviteModal,
       sendInvite,
       confirmDelete,
-      closeDeleteModal,
       deleteMember,
     };
   },
 };
 </script>
-  
-  
-  <style scoped>
-  .container-fluid {
-  padding: 0;
-  }
 
-  .card-list{
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
-    min-height: 600px;
-    width: 120vh;
-    margin: 0 1.5rem;
-  }
-  
-  /* .card {
-  margin-top: 2rem;
-  } */
-  
-  /* .card-body {
-  padding: 1rem;
-  }
-   */
-  .group-leader,
-  .group-members {
+<style scoped>
+.container-fluid {
+  padding: 0;
+}
+
+.card-list {
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  min-height: 600px;
+  width: 120vh;
+  margin: 0 1.5rem;
+}
+
+.group-leader,
+.group-members {
   margin-bottom: 1rem;
-  }
-  
-  .member-icon, .profile-icon {
+}
+
+.member-icon,
+.profile-icon {
   width: 60px;
   height: 60px;
   border-radius: 50%;
   margin-right: 2rem;
   object-fit: cover;
-  }
-  
-  .profile-name, .member-name{
+}
+
+.member-name {
   font-weight: bold;
   white-space: nowrap;
-  /* text-align: left; */
-  }
+}
 
-  .member-box, .profile-box {
+.member-box,
+.profile-box {
   display: flex;
   align-items: center;
   justify-content: left;
   margin-left: 1rem;
   position: relative;
-  /* padding: 10px; */
-  }
+}
 
-  .member-item {
-    margin-bottom: 2rem; 
-  }
-  
-  .delete-icon {
+.member-item {
+  margin-bottom: 2rem; 
+}
+
+.delete-icon {
   position: absolute;
   right: 10px; 
   cursor: pointer;
   color: red;
-  }
-  
-  .modal {
+}
+
+.modal {
   display: flex;
   position: fixed;
   top: 0;
@@ -299,42 +253,42 @@ export default {
   justify-content: center;
   align-items: center;
   background-color: rgba(0, 0, 0, 0.5);
-  }
-  
-  .modal-content {
+}
+
+.modal-content {
   background-color: white;
   padding: 20px;
   border-radius: 8px;
   width: 400px;
-  }
-  
-  .close {
+}
+
+.close {
   cursor: pointer;
   float: right;
   margin-left: auto;
-  }
-  
-  hr.horizontal {
+}
+
+hr.horizontal {
   border: 0;
   height: 1px;
   background: #ccc;
-  }
-  
-  .btn-custom {
+}
+
+.btn-custom {
   background-color: #8A9BF9;
   color: white;
-  }
-  
-  .btn-custom:hover {
+}
+
+.btn-custom:hover {
   background-color: #6f80e3;
   color: white;
-  }
+}
 
-  .card{
+.card {
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
   background-color: none;
   width: 120vh;
-  }
-  </style>
+}
+</style>
